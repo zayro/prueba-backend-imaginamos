65c77a7c12275636e745fde0a1fc3fb145bb259b
marlon zayro <zayro8905@gmail.com>
2020-06-21 14:39:25 -0500

    correciones db

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ module.exports = {
 	},
 	production: {
 	  client: 'pg',
-	  connection: process.env.PG_CONNECTION_STRING
+	  connection: process.env.DATABASE_URL
 	},
 	port: process.env.PORT || 3000,
-	db: process.env.PG_CONNECTION_STRING || 'postgres://postgres:zayro8905@localhost:5432/imaginemos',
+	db: process.env.DATABASE_URL || 'postgres://postgres:zayro8905@localhost:5432/imaginemos',
 	dblocal:  'postgres://postgres:zayro8905@localhost:5432/imaginemos',
 	dbremoto:  'postgres://wmffzqhqrxkdpj:d57ffa3a7d9cac8988441c71610e8bc49a9f75d6628b27ee43e7114a914b3e00@ec2-34-197-141-7.compute-1.amazonaws.com:5432/ddbmpbq8o1jpgg'	
 }
