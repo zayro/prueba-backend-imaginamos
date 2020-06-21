84b756538ad2548360b31ec3cff6d1d3e917fec9
marlon zayro <zayro8905@gmail.com>
2020-06-21 14:49:06 -0500

    agregando configuracion dev and prod

--- config.js
+++ config.js
@@ -1,16 +1,11 @@
 // Update with your config settings.
 
 module.exports = {
-	development: {
+	db: {
 	  client: 'pg',
-	  connection: 'postgres://postgres:zayro8905@localhost:5432/imaginamos'
+	  connection: 'postgres://postgres:zayro8905@localhost:5432/imaginamos',
 	},
-	production: {
-	  client: 'pg',
-	  connection: process.env.DATABASE_URL
-	},
-	port: process.env.PORT || 3000,
-	db: process.env.DATABASE_URL || 'postgres://postgres:zayro8905@localhost:5432/imaginemos',
-	dblocal:  'postgres://postgres:zayro8905@localhost:5432/imaginemos',
+	port: process.env.PORT || 3000,	
+	dblocal:  'postgres://postgres:zayro8905@localhost:5432/imaginamos',
 	dbremoto:  'postgres://wmffzqhqrxkdpj:d57ffa3a7d9cac8988441c71610e8bc49a9f75d6628b27ee43e7114a914b3e00@ec2-34-197-141-7.compute-1.amazonaws.com:5432/ddbmpbq8o1jpgg'	
-}
+};
